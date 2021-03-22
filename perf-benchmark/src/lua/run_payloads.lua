8cb28259261b7f4010b2c322cd08a1a879f12d87
shishqa <shishatskii.mn@phystech.edu>
2021-03-22 14:44:47 +0300

    rename script

---
+++ perf-benchmark/src/lua/run_payloads.lua
@@ -0,0 +1,58 @@
+-----------------------------------------------
+local jit = require("jit")
+local ffi = require("ffi")
+local os = require("os")
+local io = require("io")
+-----------------------------------------------
+
+benchmark_time = 0
+
+function config(args)
+    print("- config")
+
+    io.write("\t-JIT: ")
+    if args.use_jit == true then
+        jit.on()
+        print("on")
+    else
+        jit.off()
+        print("off")
+    end
+
+    benchmark_time = args.time
+    print("\t-time: "..benchmark_time.." sec");
+end
+
+-----------------------------------------------
+
+c_payload = ffi.load("lib/libc_payload.so")
+
+ffi.cdef[[
+double c_payload(double n);
+]]
+
+local payload = require("lua_payload")
+
+-- CONFIGURE HERE -----------------------------
+config{use_jit=true, time=100500}
+-----------------------------------------------
+
+print("- running...")
+
+local start_time = os.clock()
+
+local i = 0
+--   v-overhead?-v
+while os.clock() - start_time < benchmark_time do
+    if i % 2 == 0 then
+        c_payload.c_payload(15)
+    else
+        lua_payload(15)
+    end
+
+    i = i + 1
+end
+
+print("- done\n")
+
+-----------------------------------------------
