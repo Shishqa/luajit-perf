3fda31dea060d80e3ecf25936debc111ce695170
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-03-30 19:43:40 +0300

    stack fold script

---
+++ scripts/fold.py
@@ -0,0 +1,18 @@
+#!/bin/python
+import sys
+from collections import Counter
+
+argc = len(sys.argv)
+
+if argc != 2:
+    print("Usage: " + sys.argv[0] + " stack_data")
+    exit(1)
+
+with open(sys.argv[1], 'r') as stack_data:
+    lines = [x[:-2] for x in stack_data.readlines()]
+    sample_rates = dict(Counter(lines).items())
+    
+    for key, value in sample_rates.items():
+        print(key + " " + str(value))
+
+
