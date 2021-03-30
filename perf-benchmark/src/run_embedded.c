dabde4857e0810c533871f04b4a417d66fc41fc9
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-03-30 19:25:27 +0300

    luajit C profiler callback

--- perf-benchmark/src/run_embedded.c
+++ perf-benchmark/src/run_embedded.c
@@ -1,31 +1,49 @@
-#include "luajit-2.1/luajit.h"
-#include "luajit-2.1/lualib.h"
-#include "luajit-2.1/lauxlib.h"
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <unistd.h>
 
-int main(int argc, char* argv[]) 
-{
-    if (argc != 2) {
-        printf("usage: %s lua-script\n", argv[0]);
-    }
+#include "luajit-2.1/lauxlib.h"
+#include "luajit-2.1/luajit.h"
+#include "luajit-2.1/lualib.h"
 
-    printf("pid: %d\n", getpid());
+enum CONSTANTS { STACKTRACE_DEPTH = -100 };
 
-    lua_State* L = luaL_newstate();
-    assert(NULL != L);
-    luaL_openlibs(L);
+FILE* stacktrace = NULL;
 
-    int error = luaL_loadfile(L, argv[1]) || lua_pcall(L, 0, 0, 0);
-    if (error) {
-        fprintf(stderr, "%s", lua_tostring(L, -1));
-        lua_pop(L, 1);  /* pop error message from the stack */
-    }
+void profile_callback(void* data, lua_State* L, int samples, int vmstate) {
+  size_t dumpstr_len = 0;
+  const char* stack_dump =
+      luaJIT_profile_dumpstack(L, "F;", STACKTRACE_DEPTH, &dumpstr_len);
 
-    lua_close(L);
-    return 0;
+  fprintf(stacktrace, "%s\n", stack_dump);
+  fflush(stacktrace);
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    printf("usage: %s lua-script stacktrace_output\n", argv[0]);
+  }
+
+  printf("pid: %d\n", getpid());
+
+  stacktrace = fopen(argv[2], "w");
+
+  lua_State* L = luaL_newstate();
+  assert(NULL != L);
+  luaL_openlibs(L);
+
+  luaJIT_profile_start(L, "f", profile_callback, NULL);
+
+  int error = luaL_loadfile(L, argv[1]) || lua_pcall(L, 0, 0, 0);
+  if (error) {
+    fprintf(stderr, "%s", lua_tostring(L, -1));
+    lua_pop(L, 1); /* pop error message from the stack */
+  }
+
+  luaJIT_profile_stop(L);
+
+  lua_close(L);
+  fclose(stacktrace);
+  return 0;
 }
